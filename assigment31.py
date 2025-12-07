03d579d5afe1f1ff457c0ff0e78339e53ff38b91
Mitanshu1-eng <mitanshuagrawal345@gmail.com>
2025-12-07 14:22:34 +0530

    assigment 3 uploaded

---
+++ assigment31.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    f = 1
+    for i in range(1, n+1):
+        f = f * i
+    return f
+
+num = int(input("Enter a number: "))
+ans = factorial(num)
+print("Factorial of", num, "is:", ans)
---
+++ assigment32.py
@@ -0,0 +1,10 @@
+import math
+num = float(input("Enter a number: "))
+
+s = math.sqrt(num)
+l = math.log(num)
+si = math.sin(num)
+
+print("Square root:", s)
+print("Logarithm:", l)
+print("Sine:", si)
